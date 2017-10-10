2e51340621d99ea29f2013b0ac53f0250c1213d3
nicktestovv <32450934+nicktestovv@users.noreply.github.com>
2017-10-10 10:14:58 +0300

    Add files via upload

---
+++ .gitignore/kompukter.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	double q, w;
+	double sum, razn, pr, del;
+	cout << "Введите первое число: ";
+	cin >> q;
+	cout << "Введите второе число: ";
+	cin >> w;
+
+	sum = q + w;
+	razn = q - w;
+	pr = q*w;
+	del = q / w;
+
+	cout << "Сумма: ";
+	cout << sum << endl;
+	cout << "Разность: ";
+	cout << razn << endl;
+	cout << "Произведение: ";
+	cout << pr << endl;
+	cout << "Частное: ";
+	cout << del << endl;
+	system("pause");
+ 
+	return 0;
+}
